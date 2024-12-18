a6201009a8c1e3605d5c6d5e3f03e10dbe2a41e2
Aselagayan-github <aselagayan1010@gmail.com>
2024-12-18 21:27:30 +0530

    changes main page

--- resturent/urls.py
+++ resturent/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.main_page, name='main'),  # Map the root URL to the main page
+    path('menu/', views.menu, name='menu'),
+    path('blog/', views.blog, name='blog'),
+    path('contact/', views.contact, name='contact'),
 ]
--- resturent/views.py
+++ resturent/views.py
@@ -2,3 +2,13 @@ from django.shortcuts import render
 
 def main_page(request):
     return render(request, 'main.html')
+
+def menu(request):
+    return render(request, 'menu.html')
+
+def blog(request):
+    return render(request, 'blog.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
